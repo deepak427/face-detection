dbc8f0796a16f75c37251b2eaeee66c8ac5a95a6
deepak427 <62477872+deepak427@users.noreply.github.com>
2024-01-31 17:57:27 +0530

    completed project

--- main.py
+++ main.py
@@ -6,22 +6,22 @@ import cv2
 
 # Collect Images Using OpenCV
 
-# IMAGES_PATH = os.path.join('data','images')
-# number_images = 30
+IMAGES_PATH = os.path.join('data','images')
+number_images = 30
 
-# cap = cv2.VideoCapture(0)
-# for imgnum in range(number_images):
-#     print('Collecting image {}'.format(imgnum))
-#     ret, frame = cap.read()
-#     imgname = os.path.join(IMAGES_PATH,f'{str(uuid.uuid1())}.jpg')
-#     cv2.imwrite(imgname, frame)
-#     cv2.imshow('frame', frame)
-#     time.sleep(0.5)
+cap = cv2.VideoCapture(0)
+for imgnum in range(number_images):
+    print('Collecting image {}'.format(imgnum))
+    ret, frame = cap.read()
+    imgname = os.path.join(IMAGES_PATH,f'{str(uuid.uuid1())}.jpg')
+    cv2.imwrite(imgname, frame)
+    cv2.imshow('frame', frame)
+    time.sleep(0.5)
 
-#     if cv2.waitKey(1) & 0xFF == ord('q'):
-#         break
-# cap.release()
-# cv2.destroyAllWindows()
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+cap.release()
+cv2.destroyAllWindows()
 
 # Annotate images with LabelMe
 
@@ -40,28 +40,28 @@ def load_image(x):
     img = tf.io.decode_jpeg(byte_img)
     return img
 
-# images = images.map(load_image)
+images = images.map(load_image)
 
 # Visualizing data
 
-# image_generator = images.batch(4).as_numpy_iterator()
-# plot_images = image_generator.next()
+image_generator = images.batch(4).as_numpy_iterator()
+plot_images = image_generator.next()
 
-# fig, ax = plt.subplots(ncols=4, figsize=(20,20))
-# for idx, image in enumerate(plot_images):
-#     ax[idx].imshow(image)
-# plt.show()
+fig, ax = plt.subplots(ncols=4, figsize=(20,20))
+for idx, image in enumerate(plot_images):
+    ax[idx].imshow(image)
+plt.show()
 
 # Partition unaugmented data
 
-# for folder in ['train', 'test', 'val']:
-#     for file in os.listdir(os.path.join('data', folder, 'images')):
+for folder in ['train', 'test', 'val']:
+    for file in os.listdir(os.path.join('data', folder, 'images')):
 
-#         filename = file.split('.')[0]+'.json'
-#         existing_filepath = os.path.join('data', 'labels', filename)
-#         if os.path.exists(existing_filepath):
-#             new_filepath = os.path.join('data', folder, 'labels', filename)
-#             os.replace(existing_filepath, new_filepath)
+        filename = file.split('.')[0]+'.json'
+        existing_filepath = os.path.join('data', 'labels', filename)
+        if os.path.exists(existing_filepath):
+            new_filepath = os.path.join('data', folder, 'labels', filename)
+            os.replace(existing_filepath, new_filepath)
 
 # Apply Image Augmentation on Images and Labels using Albumentations
 
@@ -75,73 +75,73 @@ augmentor = alb.Compose([alb.RandomCrop(width=450, height=450),
                         bbox_params=alb.BboxParams(format='albumentations',
                                                    label_fields=['class_labels']))
 
-# img = cv2.imread(os.path.join('data', 'train', 'images',
-#                  '2a0cb89e-bf63-11ee-83e3-a7b6846881c1.jpg'))
-# with open(os.path.join('data', 'train', 'labels', '2a0cb89e-bf63-11ee-83e3-a7b6846881c1.json'), 'r') as f:
-#     label = json.load(f)
+img = cv2.imread(os.path.join('data', 'train', 'images',
+                 '2a0cb89e-bf63-11ee-83e3-a7b6846881c1.jpg'))
+with open(os.path.join('data', 'train', 'labels', '2a0cb89e-bf63-11ee-83e3-a7b6846881c1.json'), 'r') as f:
+    label = json.load(f)
 
-# coords = [0, 0, 0, 0]
-# coords[0] = label['shapes'][0]['points'][0][0]
-# coords[1] = label['shapes'][0]['points'][0][1]
-# coords[2] = label['shapes'][0]['points'][1][0]
-# coords[3] = label['shapes'][0]['points'][1][1]
+coords = [0, 0, 0, 0]
+coords[0] = label['shapes'][0]['points'][0][0]
+coords[1] = label['shapes'][0]['points'][0][1]
+coords[2] = label['shapes'][0]['points'][1][0]
+coords[3] = label['shapes'][0]['points'][1][1]
 
-# coords = list(np.divide(coords, [640, 480, 640, 480]))
+coords = list(np.divide(coords, [640, 480, 640, 480]))
 
-# augmented = augmentor(image=img, bboxes=[coords], class_labels=['face'])
+augmented = augmentor(image=img, bboxes=[coords], class_labels=['face'])
 
-# cv2.rectangle(augmented['image'],
-#               tuple(np.multiply(augmented['bboxes']
-#                     [0][:2], [450, 450]).astype(int)),
-#               tuple(np.multiply(augmented['bboxes']
-#                     [0][2:], [450, 450]).astype(int)),
-#               (255, 0, 0), 2)
+cv2.rectangle(augmented['image'],
+              tuple(np.multiply(augmented['bboxes']
+                    [0][:2], [450, 450]).astype(int)),
+              tuple(np.multiply(augmented['bboxes']
+                    [0][2:], [450, 450]).astype(int)),
+              (255, 0, 0), 2)
 
-# plt.imshow(augmented['image'])
+plt.imshow(augmented['image'])
 
 # Run augmentation pipeline
 
-# for partition in ['train','test','val']: 
-#     for image in os.listdir(os.path.join('data', partition, 'images')):
-#         img = cv2.imread(os.path.join('data', partition, 'images', image))
+for partition in ['train','test','val']: 
+    for image in os.listdir(os.path.join('data', partition, 'images')):
+        img = cv2.imread(os.path.join('data', partition, 'images', image))
 
-#         coords = [0,0,0.00001,0.00001]
-#         label_path = os.path.join('data', partition, 'labels', f'{image.split(".")[0]}.json')
-#         if os.path.exists(label_path):
-#             with open(label_path, 'r') as f:
-#                 label = json.load(f)
+        coords = [0,0,0.00001,0.00001]
+        label_path = os.path.join('data', partition, 'labels', f'{image.split(".")[0]}.json')
+        if os.path.exists(label_path):
+            with open(label_path, 'r') as f:
+                label = json.load(f)
 
-#             coords[0] = label['shapes'][0]['points'][0][0]
-#             coords[1] = label['shapes'][0]['points'][0][1]
-#             coords[2] = label['shapes'][0]['points'][1][0]
-#             coords[3] = label['shapes'][0]['points'][1][1]
-#             coords = list(np.divide(coords, [640,480,640,480]))
+            coords[0] = label['shapes'][0]['points'][0][0]
+            coords[1] = label['shapes'][0]['points'][0][1]
+            coords[2] = label['shapes'][0]['points'][1][0]
+            coords[3] = label['shapes'][0]['points'][1][1]
+            coords = list(np.divide(coords, [640,480,640,480]))
 
-#         try: 
-#             for x in range(60):
-#                 augmented = augmentor(image=img, bboxes=[coords], class_labels=['face'])
-#                 cv2.imwrite(os.path.join('aug_data', partition, 'images', f'{image.split(".")[0]}.{x}.jpg'), augmented['image'])
+        try: 
+            for x in range(60):
+                augmented = augmentor(image=img, bboxes=[coords], class_labels=['face'])
+                cv2.imwrite(os.path.join('aug_data', partition, 'images', f'{image.split(".")[0]}.{x}.jpg'), augmented['image'])
 
-#                 annotation = {}
-#                 annotation['image'] = image
+                annotation = {}
+                annotation['image'] = image
 
-#                 if os.path.exists(label_path):
-#                     if len(augmented['bboxes']) == 0: 
-#                         annotation['bbox'] = [0,0,0,0]
-#                         annotation['class'] = 0 
-#                     else: 
-#                         annotation['bbox'] = augmented['bboxes'][0]
-#                         annotation['class'] = 1
-#                 else: 
-#                     annotation['bbox'] = [0,0,0,0]
-#                     annotation['class'] = 0 
+                if os.path.exists(label_path):
+                    if len(augmented['bboxes']) == 0: 
+                        annotation['bbox'] = [0,0,0,0]
+                        annotation['class'] = 0 
+                    else: 
+                        annotation['bbox'] = augmented['bboxes'][0]
+                        annotation['class'] = 1
+                else: 
+                    annotation['bbox'] = [0,0,0,0]
+                    annotation['class'] = 0 
 
 
-#                 with open(os.path.join('aug_data', partition, 'labels', f'{image.split(".")[0]}.{x}.json'), 'w') as f:
-#                     json.dump(annotation, f)
+                with open(os.path.join('aug_data', partition, 'labels', f'{image.split(".")[0]}.{x}.json'), 'w') as f:
+                    json.dump(annotation, f)
 
-#         except Exception as e:
-#             print(e)
+        except Exception as e:
+            print(e)
 
 # Load augmented images to tensorflow dataset
 
@@ -241,6 +241,98 @@ def build_model():
 
 facetracker = build_model()
 
+# Define losses and optimizers
 
- 
+opt = tf.keras.optimizers.Adam(learning_rate=0.0001)
 
+def localization_loss(y_true, yhat):            
+    delta_coord = tf.reduce_sum(tf.square(y_true[:,:2] - yhat[:,:2]))
+                  
+    h_true = y_true[:,3] - y_true[:,1] 
+    w_true = y_true[:,2] - y_true[:,0] 
+
+    h_pred = yhat[:,3] - yhat[:,1] 
+    w_pred = yhat[:,2] - yhat[:,0] 
+    
+    delta_size = tf.reduce_sum(tf.square(w_true - w_pred) + tf.square(h_true-h_pred))
+    
+    return delta_coord + delta_size
+classloss = tf.keras.losses.BinaryCrossentropy()
+regressloss = localization_loss
+
+# Create custom model class
+
+class FaceTracker(Model): 
+    def __init__(self, eyetracker,  **kwargs): 
+        super().__init__(**kwargs)
+        self.model = eyetracker
+
+    def compile(self, opt, classloss, localizationloss, **kwargs):
+        super().compile(**kwargs)
+        self.closs = classloss
+        self.lloss = localizationloss
+        self.opt = opt
+    
+    def train_step(self, batch, **kwargs): 
+        
+        X, y = batch
+        
+        with tf.GradientTape() as tape: 
+            classes, coords = self.model(X, training=True)
+            
+            batch_classloss = self.closs(y[0], classes)
+            batch_localizationloss = self.lloss(tf.cast(y[1], tf.float32), coords)
+            
+            total_loss = batch_localizationloss+0.5*batch_classloss
+            
+            grad = tape.gradient(total_loss, self.model.trainable_variables)
+        
+        opt.apply_gradients(zip(grad, self.model.trainable_variables))
+        
+        return {"total_loss":total_loss, "class_loss":batch_classloss, "regress_loss":batch_localizationloss}
+    
+    def test_step(self, batch, **kwargs): 
+        X, y = batch
+        
+        classes, coords = self.model(X, training=False)
+        
+        batch_classloss = self.closs(y[0], classes)
+        batch_localizationloss = self.lloss(tf.cast(y[1], tf.float32), coords)
+        total_loss = batch_localizationloss+0.5*batch_classloss
+        
+        return {"total_loss":total_loss, "class_loss":batch_classloss, "regress_loss":batch_localizationloss}
+        
+    def call(self, X, **kwargs): 
+        return self.model(X, **kwargs)
+    
+model = FaceTracker(facetracker)
+model.compile(opt, classloss, regressloss)
+
+logdir='logs'
+tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=logdir)
+hist = model.fit(train, epochs=40, validation_data=val, callbacks=[tensorboard_callback])
+
+# Plot performance
+
+fig, ax = plt.subplots(ncols=3, figsize=(20,5))
+
+ax[0].plot(hist.history['total_loss'], color='teal', label='loss')
+ax[0].plot(hist.history['val_total_loss'], color='orange', label='val loss')
+ax[0].title.set_text('Loss')
+ax[0].legend()
+
+ax[1].plot(hist.history['class_loss'], color='teal', label='class loss')
+ax[1].plot(hist.history['val_class_loss'], color='orange', label='val class loss')
+ax[1].title.set_text('Classification Loss')
+ax[1].legend()
+
+ax[2].plot(hist.history['regress_loss'], color='teal', label='regress loss')
+ax[2].plot(hist.history['val_regress_loss'], color='orange', label='val regress loss')
+ax[2].title.set_text('Regression Loss')
+ax[2].legend()
+
+plt.show()
+
+#save model
+
+facetracker.save('facetracker.h5')
